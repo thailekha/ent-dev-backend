4b3c4396374cda9d11fad3207216cbf8a5ff4db5
Thai Le <thailekha@yahoo.com>
2018-04-23 18:28:21 +0100

    cleanup

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`);
 });
 
-// print mongoose logs in dev env
 if (config.mongooseDebug) {
   mongoose.set('debug', (collectionName, method, query, doc) => {
     debug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc);
